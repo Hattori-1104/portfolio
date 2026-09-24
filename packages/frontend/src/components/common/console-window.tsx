171a08684df16f0e8a9e65865bf61cd5422d047d
hattori-expertbook <hattori.index.js@gmail.com>
2026-09-24 14:44:39 +0900

    fix: bashウィンドウのタイトルをstickyに

--- packages/frontend/src/components/common/console-window.tsx
+++ packages/frontend/src/components/common/console-window.tsx
@@ -11,8 +11,10 @@ export function ConsoleWindow({
 }) {
 	const windowName = windowMessage ? `bash - ${windowMessage}` : "bash"
 	return (
-		<div className="flex flex-col border shadow overflow-x-auto">
-			<div className="bg-white min-w-100 w-full px-2 py-1">{windowName}</div>
+		<div className="flex flex-col border shadow overflow-x-auto relative">
+			<div className="bg-white w-full px-2 py-1 sticky left-0">
+				{windowName}
+			</div>
 			<div className="bg-black min-w-100 w-full text-white p-2">{children}</div>
 		</div>
 	)
